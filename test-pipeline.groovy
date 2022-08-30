25bd7c3e221106b8e46cdd788093d0aa6a7c0051
fsit-arun <111054171+fsit-arun@users.noreply.github.com>
2022-08-30 10:11:32 +0530

    Add files via upload

---
+++ test-pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline{
+    agent any
+    stages{
+        stage("one"){
+            steps {
+                echo "stage one Done."
+            }
+        }
+        stage("whoami"){
+            steps {
+                sh "whoami"
+            }
+        }
+        stage("hostname"){
+            steps {
+                sh "whoami"
+            }
+        }
+        stage("ip"){
+            steps {
+                sh "ip a"
+            }
+        }
+    }
+}
